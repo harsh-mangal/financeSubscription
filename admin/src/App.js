aeadc56d944e9c5e26c62eaf015e96cc0c13fe81
Harsh Mangal <116105912+harsh-mangal@users.noreply.github.com>
2025-10-31 12:42:04 +0530

    setuped

---
+++ admin/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     this is admin
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     this is client
+    </div>
+  );
+}
+
+export default App;
